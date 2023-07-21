61c4334d8f5b859cbcd6733995913f392fa2c40d
WAShannon <washannon@gmail.com>
2023-07-21 16:56:08 -0400

    Temperature response to ambient. FAN dimmer.

--- rpi4tower_leds.py
+++ rpi4tower_leds.py
@@ -2,12 +2,14 @@ import board
 import neopixel
 import time
 import random
+import subprocess
 
 # LED strip configuration
 LED_COUNT = 5           # Number of LED pixels
 LED_PIN = board.D18     # GPIO pin connected to the LED strip (GPIO18)
-LED_BRIGHTNESS = 0.2    # Brightness (0.0 to 1.0)
+LED_BRIGHTNESS = 0.5    # Brightness (0.0 to 1.0)
 LED_ORDER = neopixel.GRB  # Strip color order (GRB for WS2812B)
+LED_DIMMER = 0.25
 
 # Initialize the LED strip
 strip = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, auto_write=False, pixel_order=LED_ORDER)
@@ -21,6 +23,15 @@ def turn_off_leds():
     """Turn off all LEDs."""
     for i in range(LED_COUNT):
         set_led_color(i, 0, 0, 0)
+
+# Function to get CPU temperature using vcgencmd
+def get_cpu_temperature():
+    try:
+        cpu_temp_str = subprocess.check_output(['vcgencmd', 'measure_temp']).decode()
+        cpu_temp = float(cpu_temp_str.split('=')[1].split('\'')[0])
+        return cpu_temp
+    except:
+        return "N/A"
 
 # Example usage
 if __name__ == "__main__":
@@ -40,11 +51,17 @@ if __name__ == "__main__":
 
         while True:
             for i in range(1,4):
-                        r = random.randint(0,255)
-                        g = random.randint(0,255)
-                        b = random.randint(0,255)
+                        r = random.randint(0,int(255 * LED_DIMMER))
+                        g = random.randint(0,int(255 * LED_DIMMER))
+                        b = random.randint(0,int(255 * LED_DIMMER))
                         set_led_color(i, r, g, b)
                         time.sleep(1)
+                        if get_cpu_temperature() < 45:
+                            set_led_color(0, 0, 0, 255) # Blue
+                        elif get_cpu_temperature() > 60:
+                            set_led_color(0,255,0,0) # Red
+                        else:
+                            set_led_color(0,255,0,255) # Purple  
 
 
     except KeyboardInterrupt:
