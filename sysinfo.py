5a53b763b0d1650bcace0d83acc13bdd8bfd7323
WAShannon <washannon@gmail.com>
2023-07-21 14:37:31 -0400

    Basic info page complete

--- sysinfo.py
+++ sysinfo.py
@@ -7,11 +7,6 @@ import netifaces
 import subprocess
 from PIL import Image, ImageDraw, ImageFont
 
-# OLED display setup
-reset_pin = digitalio.DigitalInOut(board.D4)
-i2c = board.I2C()
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C, reset=reset_pin)
-
 # Function to get the current IP address
 def get_ip_address(interface='wlan0'):
     try:
@@ -39,28 +34,30 @@ def get_memory_usage():
     memory = psutil.virtual_memory()
     return memory.percent
 
-# Function to get disk usage for each actual disk with the drive name (excluding /boot)
+# Function to get disk usage for the root filesystem
 def get_disk_usage():
-    disk_info = []
     partitions = psutil.disk_partitions(all=False)  # Exclude all virtual filesystems
     for partition in partitions:
-        drive_name = partition.device.split("/")[-1]
-        if drive_name == "mmcblk0p1":  # Rename the boot partition (if needed)
-            drive_name = "boot"
-        if partition.mountpoint != "/boot":  # Exclude the /boot partition
+        if partition.mountpoint == "/":
             try:
                 usage = psutil.disk_usage(partition.mountpoint)
-                disk_info.append((drive_name, usage.used, usage.percent))
+                return (partition.device.split("/")[-1], usage.used, usage.percent)
             except:
                 pass
-    return disk_info
+    return None
+
+# OLED display setup
+reset_pin = digitalio.DigitalInOut(board.D4)
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C, reset=reset_pin)
 
 # Clear the display once at the beginning
 oled.fill(0)
 oled.show()
 
-# Load the bold font for text
-font_bold = ImageFont.truetype("DejaVuSans-Bold.ttf", 10)
+# Load the bold and regular fonts for text
+font_bold_10 = ImageFont.truetype("DejaVuSans-Bold.ttf", 10)
+font_bold_14 = ImageFont.truetype("DejaVuSans-Bold.ttf", 14)
 
 try:
     while True:
@@ -70,31 +67,32 @@ try:
 
         # Get and display the total CPU usage
         total_cpu_usage = get_total_cpu_usage()
-        draw.text((0, 0), f"Total CPU: {total_cpu_usage:.1f}%", font=font_bold, fill=1)  # 1 for white color
+        draw.text((0, 0), f"Total CPU: {total_cpu_usage:.1f}%", font=font_bold_10, fill=1)  # 1 for white color
 
         # Get and display the CPU temperature
         cpu_temp = get_cpu_temperature()
         if cpu_temp != "N/A":
-            draw.text((0, 12), f"CPU Temp: {cpu_temp:.1f} \u00b0C", font=font_bold, fill=1)  # 1 for white color
+            draw.text((0, 12), f"CPU Temp: {cpu_temp:.1f} \u00b0C", font=font_bold_10, fill=1)  # 1 for white color
         else:
-            draw.text((0, 12), "CPU Temp: N/A", font=font_bold, fill=1)  # 1 for white color
+            draw.text((0, 12), "CPU Temp: N/A", font=font_bold_10, fill=1)  # 1 for white color
 
         # Get and display the memory (RAM) usage
         memory_usage = get_memory_usage()
-        draw.text((0, 24), f"MEM: {memory_usage:.1f}%", font=font_bold, fill=1)  # 1 for white color
+        draw.text((0, 24), f"MEM: {memory_usage:.1f}%", font=font_bold_10, fill=1)  # 1 for white color
 
-        # Get and display the disk usage for each actual disk with the drive name (excluding /boot)
+        # Get and display the disk usage for the root filesystem
         disk_info = get_disk_usage()
-        for i, (drive_name, used, disk_usage) in enumerate(disk_info):
-            draw.text((0, 36 + i * 12), f"{drive_name}: {used / (1024 ** 3):.1f}GB ({disk_usage:.1f}%)", font=font_bold, fill=1)  # 1 for white color
+        if disk_info is not None:
+            drive_name, used, disk_usage = disk_info
+            drive_text = f"{drive_name}: {used / (1024 ** 3):.1f}GB ({disk_usage:.1f}%)"
+            draw.text((0, 36), drive_text, font=font_bold_10, fill=1)  # 1 for white color
 
         # Get and display the network IP at the bottom with a larger font
         ip_address = get_ip_address()
-        font_ip = ImageFont.truetype("DejaVuSans-Bold.ttf", 14)  # Larger font for IP address
-        ip_text_width, ip_text_height = draw.textsize(ip_address, font=font_ip)
-        ip_x = (oled.width - ip_text_width) // 2
-        ip_y = oled.height - ip_text_height
-        draw.text((ip_x, ip_y), ip_address, font=font_ip, fill=1)  # 1 for white color
+        ip_text_size = draw.textsize(ip_address, font=font_bold_14)
+        ip_x = (oled.width - ip_text_size[0]) // 2
+        ip_y = oled.height - ip_text_size[1] - 1
+        draw.text((ip_x, ip_y), ip_address, font=font_bold_14, fill=1)  # 1 for white color
 
         # Display the image on the OLED
         oled.image(image)
